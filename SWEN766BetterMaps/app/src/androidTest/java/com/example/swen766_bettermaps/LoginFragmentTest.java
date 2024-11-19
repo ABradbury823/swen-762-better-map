8a9427f3c63d84276dc5cb1329a328aa63e895bc
Tyler <txj3401@glados.cs.rit.edu>
2024-11-18 19:10:10 -0500

    Implemented test for navigating to login screen.

--- SWEN766BetterMaps/app/src/androidTest/java/com/example/swen766_bettermaps/LoginFragmentTest.java
+++ SWEN766BetterMaps/app/src/androidTest/java/com/example/swen766_bettermaps/LoginFragmentTest.java
@@ -1,6 +1,5 @@
 package com.example.swen766_bettermaps;
 
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Rule;
@@ -9,15 +8,16 @@ import org.junit.Test;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class LoginFragmentTest {
     @Rule
     public ActivityScenarioRule<MainActivity> mainActivityScenario = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void clickLoginWithGoogleButton() {
-        onView(withId(R.id.sign_in_button)).perform(click());
+    public void checkLoginNavigation() {
+        onView(withId(R.id.navigation_account)).perform(click());
+        onView(withId(R.id.sign_in_button)).check(matches(isDisplayed()));
     }
 }
