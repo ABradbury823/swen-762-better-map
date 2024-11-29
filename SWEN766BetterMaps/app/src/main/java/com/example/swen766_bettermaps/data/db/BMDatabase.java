964d6f11bf49a2f33faa10ec863b662864e682de
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-11-29 14:27:11 -0500

    added location dao to db class

--- SWEN766BetterMaps/app/src/main/java/com/example/swen766_bettermaps/data/db/BMDatabase.java
+++ SWEN766BetterMaps/app/src/main/java/com/example/swen766_bettermaps/data/db/BMDatabase.java
@@ -3,17 +3,21 @@ package com.example.swen766_bettermaps.data.db;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.example.swen766_bettermaps.data.db.daos.LocationDAO;
 import com.example.swen766_bettermaps.data.db.daos.UserDAO;
+import com.example.swen766_bettermaps.data.db.entities.Location;
 import com.example.swen766_bettermaps.data.db.entities.User;
 
 /**
  * A database using Room for the BetterMaps application.
  * <br>Initialize the singleton instance with <code>Room.databaseBuilder()</code>
  */
-@Database(entities = {User.class}, version = 1, exportSchema = false)
+@Database(entities = {User.class, Location.class}, version = 1, exportSchema = false)
 public abstract class BMDatabase extends RoomDatabase {
 
     public static final String DATABASE_NAME = "bm_database";
 
     public abstract UserDAO userDAO();
+
+    public abstract LocationDAO locationDAO();
 }
