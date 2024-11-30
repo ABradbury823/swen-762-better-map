e1927c154bea7952f17618ec551c28e0e98a1844
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-11-29 20:15:04 -0500

    amenity dao

---
+++ SWEN766BetterMaps/app/src/main/java/com/example/swen766_bettermaps/data/db/daos/AmenityDAO.java
@@ -0,0 +1,55 @@
+package com.example.swen766_bettermaps.data.db.daos;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.swen766_bettermaps.data.db.entities.Amenity;
+
+import java.util.List;
+
+/**
+ * A Data Access Object containing methods for accessing the amenities table.
+ */
+@Dao
+public interface AmenityDAO {
+
+    /**
+     * Inserts a new amenity into the amenities table.
+     * @param amenity The new amenity.
+     */
+    @Insert
+    void insert(Amenity amenity);
+
+    /**
+     * Retrieves all amenities from the amenities table.
+     * @return A list containing all amenities.
+     */
+    @Query("SELECT * FROM amenities")
+    List<Amenity> getAllAmenities();
+
+    /**
+     * Retrieves an amenity from the amenities table based on its id.
+     * @param amenityId The amenity's id.
+     * @return The amenity that matches the id,
+     * or null if the id does not match to an amenity.
+     */
+    @Query("SELECT * FROM amenities WHERE id = :amenityId")
+    Amenity getAmenityById(int amenityId);
+
+    /**
+     * Updates an amenity in the amenities table.
+     * @param amenity The amenity to update. Primary key must match to an existing amenity.
+     */
+    @Update
+    void update(Amenity amenity);
+
+    /**
+     * Removes an amenity from the amenities table.
+     * @param amenity The amenity to update. Primary key must match to an existing amenities.
+     */
+    @Delete
+    void delete(Amenity amenity);
+}
