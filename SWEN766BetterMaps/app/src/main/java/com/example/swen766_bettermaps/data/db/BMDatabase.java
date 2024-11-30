69173149015d7b400a388cc44554f453d3ddefe9
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-11-29 21:55:55 -0500

    added all entities to db class

--- SWEN766BetterMaps/app/src/main/java/com/example/swen766_bettermaps/data/db/BMDatabase.java
+++ SWEN766BetterMaps/app/src/main/java/com/example/swen766_bettermaps/data/db/BMDatabase.java
@@ -5,14 +5,24 @@ import androidx.room.RoomDatabase;
 
 import com.example.swen766_bettermaps.data.db.daos.LocationDAO;
 import com.example.swen766_bettermaps.data.db.daos.UserDAO;
+import com.example.swen766_bettermaps.data.db.entities.Amenity;
 import com.example.swen766_bettermaps.data.db.entities.Location;
+import com.example.swen766_bettermaps.data.db.entities.LocationAmenity;
 import com.example.swen766_bettermaps.data.db.entities.User;
+import com.example.swen766_bettermaps.data.db.entities.UserFavoriteLocation;
 
 /**
  * A database using Room for the BetterMaps application.
  * <br>Initialize the singleton instance with <code>Room.databaseBuilder()</code>
  */
-@Database(entities = {User.class, Location.class}, version = 1, exportSchema = false)
+@Database(entities = {
+    User.class,
+    UserFavoriteLocation.class,
+    Location.class,
+    LocationAmenity.class,
+    Amenity.class
+},
+    version = 1, exportSchema = false)
 public abstract class BMDatabase extends RoomDatabase {
 
     public static final String DATABASE_NAME = "bm_database";
