0cb1766238797fe703eca0773a31d5939fa3b539
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-12-01 17:07:22 -0500

    test get all users

--- SWEN766BetterMaps/app/src/androidTest/java/com/example/swen766_bettermaps/db/daos/UserDAOTest.java
+++ SWEN766BetterMaps/app/src/androidTest/java/com/example/swen766_bettermaps/db/daos/UserDAOTest.java
@@ -17,6 +17,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 public class UserDAOTest {
     private BMDatabase database;
     private UserDAO userDAO;
@@ -54,6 +56,33 @@ public class UserDAOTest {
         assertEquals(user.getUsername(), retrievedUser.getUsername());
         assertEquals(user.getEmail(), retrievedUser.getEmail());
         assertEquals(user.getRole(), retrievedUser.getRole());
+    }
+
+    // TODO: test insert favorite location (test Location first)
+
+    /**
+     * Tests that getAllUsers retrieves a list of every user.
+     */
+    @Test
+    public void testGetAllUsers() {
+        User[] users = {
+            new User("User 1", "user1@email.com", UserRole.STUDENT),
+            new User("User 2", "user2@email.com", UserRole.FACULTY),
+            new User("User 3", "user3@email.com", UserRole.ADMIN)
+        };
+        for(User u : users) {
+            userDAO.insert(u);
+        }
+
+        List<User> retrievedUsers = userDAO.getAllUsers();
+
+        assertNotNull(retrievedUsers);
+        assertEquals(users.length, retrievedUsers.size());
+        for(int i = 0; i < retrievedUsers.size(); i++) {
+            assertEquals(users[i].getUsername(), retrievedUsers.get(i).getUsername());
+            assertEquals(users[i].getEmail(), retrievedUsers.get(i).getEmail());
+            assertEquals(users[i].getRole(), retrievedUsers.get(i).getRole());
+        }
     }
 
 
