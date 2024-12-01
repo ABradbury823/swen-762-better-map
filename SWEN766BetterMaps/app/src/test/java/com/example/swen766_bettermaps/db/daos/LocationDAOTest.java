9007a3b0428860a6d3219e6af0c9ce6d0a83985a
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-11-30 21:52:58 -0500

    db setup for tests

--- SWEN766BetterMaps/app/src/test/java/com/example/swen766_bettermaps/db/daos/LocationDAOTest.java
+++ SWEN766BetterMaps/app/src/test/java/com/example/swen766_bettermaps/db/daos/LocationDAOTest.java
@@ -1,2 +1,35 @@
-package com.example.swen766_bettermaps.db.daos;public class LocationDAOTest {
+package com.example.swen766_bettermaps.db.daos;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.example.swen766_bettermaps.data.db.BMDatabase;
+import com.example.swen766_bettermaps.data.db.daos.LocationDAO;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class LocationDAOTest {
+    private BMDatabase database;
+    private LocationDAO locationDAO;
+
+    // create db before each test
+    @Before
+    public void setUp() {
+        // create in-memory database for testing
+        Context context = ApplicationProvider.getApplicationContext();
+        database = Room.inMemoryDatabaseBuilder(context, BMDatabase.class)
+            .allowMainThreadQueries() // main thread use for testing only
+            .build();
+
+        locationDAO = database.locationDAO();
+    }
+
+    // clean up db after each test
+    @After
+    public void tearDown() {
+        database.close();
+    }
 }
