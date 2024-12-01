3b6227a004e09b5021b0064145833e5cd3788d49
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-11-30 19:55:59 -0500

    test from user role

--- SWEN766BetterMaps/app/src/test/java/com/example/swen766_bettermaps/db/types/UserRoleConverterTest.java
+++ SWEN766BetterMaps/app/src/test/java/com/example/swen766_bettermaps/db/types/UserRoleConverterTest.java
@@ -1,2 +1,24 @@
-package com.example.swen766_bettermaps.db.types;public class UserRoleConverterTest {
+package com.example.swen766_bettermaps.db.types;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.swen766_bettermaps.data.db.types.UserRole;
+import com.example.swen766_bettermaps.data.db.types.UserRoleConverter;
+
+import org.junit.Test;
+
+public class UserRoleConverterTest {
+
+    /**
+     * Tests that fromUserRole converts a UserRole to a String.
+     */
+    @Test
+    public void testFromUserRole() {
+        UserRole role = UserRole.ADMIN;
+        String expectedStr = role.name();
+
+        String actualStr = UserRoleConverter.fromUserRole(role);
+        assertEquals(expectedStr, actualStr);
+    }
+
 }
