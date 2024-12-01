dfd241256338c59ebc43ad4bfff9d065c06d009a
Andrew <70240081+ABradbury823@users.noreply.github.com>
2024-11-30 19:28:37 -0500

    to coordinate invalid arg tests

--- SWEN766BetterMaps/app/src/test/java/com/example/swen766_bettermaps/db/types/CoordinateConverterTest.java
+++ SWEN766BetterMaps/app/src/test/java/com/example/swen766_bettermaps/db/types/CoordinateConverterTest.java
@@ -1,6 +1,7 @@
 package com.example.swen766_bettermaps.db.types;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import com.example.swen766_bettermaps.data.db.types.Coordinate;
 import com.example.swen766_bettermaps.data.db.types.CoordinateConverter;
@@ -28,13 +29,57 @@ public class CoordinateConverterTest {
      */
     @Test
     public void testToCoordinate() {
-        float lat = 39.85f;
+        float lat = -39.85f;
         float lan = 81.2f;
         Coordinate expectedCoordinate = new Coordinate(lat, lan);
         String coordinateStr = CoordinateConverter.fromCoordinate(expectedCoordinate);
 
         Coordinate actualCoordinate = CoordinateConverter.toCoordinate(coordinateStr);
         assertEquals(expectedCoordinate, actualCoordinate);
+    }
+
+    /**
+     * Tests that toCoordinate returns null if the data has too few arguments (one value/bad delimiter)
+     */
+    @Test
+    public void testToCoordinateOneArg() {
+        String coordinateStr = "111.234";
+
+        Coordinate coordinate = CoordinateConverter.toCoordinate(coordinateStr);
+        assertNull(coordinate);
+
+
+        coordinateStr = "50.23|21.5";
+
+        coordinate = CoordinateConverter.toCoordinate(coordinateStr);
+        assertNull(coordinate);
+    }
+
+    /**
+     * Tests that toCoordinate returns null if too many arguments are given.
+     */
+    @Test
+    public void testToCoordinateExtraArgs() {
+        String coordinateStr = "1.008,2.115,3.1415";
+
+        Coordinate coordinate = CoordinateConverter.toCoordinate(coordinateStr);
+        assertNull(coordinate);
+    }
+
+    /**
+     * Tests that toCoordinate returns null if either value cannot be parsed to a float.
+     */
+    @Test
+    public void testToCoordinateNaN() {
+        String coordinateStr = "; SELECT * FROM users; --,44.4";
+
+        Coordinate coordinate = CoordinateConverter.toCoordinate(coordinateStr);
+        assertNull(coordinate);
+
+        coordinateStr = "1.008,Twenty-Nine";
+
+        coordinate = CoordinateConverter.toCoordinate(coordinateStr);
+        assertNull(coordinate);
     }
 
 }
